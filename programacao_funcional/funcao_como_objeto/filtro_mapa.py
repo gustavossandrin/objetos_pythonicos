8f935745093f35bab52e62561528016488ada8b7
Pichau <gustavosandrindattein123@gmail.com>
2022-06-29 15:08:12 -0300

    módulo operator

--- programacao_funcional/funcao_como_objeto/filtro_mapa.py
+++ programacao_funcional/funcao_como_objeto/filtro_mapa.py
@@ -1,3 +1,5 @@
+import operator
+
 alunos = [('gustavo', 6), ('guilherme', 8), ('jessica', 4)]
 
 print([nome for nome, nota in alunos if nota > 5])
@@ -9,11 +11,4 @@ def possui_nota_maior_que_5(aluno):
 
 
 print(list(filter(possui_nota_maior_que_5, alunos)))
-
-
-def extrair_nome(aluno):
-    nome, _ = aluno
-    return nome
-
-
-print(list(map(extrair_nome, filter(possui_nota_maior_que_5, alunos))))
+print(list(map(operator.itemgetter(0), filter(possui_nota_maior_que_5, alunos))))
