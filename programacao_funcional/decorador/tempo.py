fb55bb073762fe6003d12efd04a3f5ed04e7284a
Pichau <gustavosandrindattein123@gmail.com>
2022-06-30 23:29:23 -0300

    decorator com parametro

--- programacao_funcional/decorador/tempo.py
+++ programacao_funcional/decorador/tempo.py
@@ -1,27 +1,34 @@
+from functools import wraps
 from time import sleep, strftime
 
 
-def logar(f):
-    def executar_com_tempo(*args, **kwargs):
-        print(strftime('%H:%M:%S'))
-        return f(*args, **kwargs)
+def logar(fmt='%H:%M:%S'):
+    def decorator(f):
+        @wraps(f)
+        def executar_com_tempo(*args, **kwargs):
+            print(strftime(fmt))
+            return f(*args, **kwargs)
 
-    return executar_com_tempo
+        return executar_com_tempo
+
+    return decorator
 
 
-@logar
+@logar(fmt='%H:%M:%S')
 def mochileiro():
     return 42
 
 
-@logar
+@logar(fmt='%d/%m/%Y %H:%M:%S')
 def ola(nome):
     return f'ola {nome}'
 
 
 if __name__ == '__main__':
     print(mochileiro())
+    print(mochileiro.__name__)
     print(ola('gustavo'))
+    print(ola.__name__)
     sleep(1)
     print(mochileiro())
     print(ola('renzo'))
